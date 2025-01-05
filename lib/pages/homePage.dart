6624740732123a1b3d154732dc3a98a6c774ae8a
Saksham <sakshamloona9@gmail.com>
2025-01-05 11:49:59 +0530

    Added HomePage for the Application

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Homepage();
+  }
+}
+
+class _Homepage extends State<Homepage> {
+  _Homepage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
